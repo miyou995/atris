07fa1e7fb22571b194e62716c04930f2f52c34ea
yousra <yousra09blida1995@gmail.com>
2025-12-11 16:55:46 +0100

    added dexon

--- home/templatetags/navigation_tags.py
+++ home/templatetags/navigation_tags.py
@@ -11,4 +11,12 @@ register = template.Library()
 # ... keep the definition of get_footer_text and add the get_site_root template tag:
 @register.simple_tag(takes_context=True)
 def get_site_root(context):
+    nav =Site.find_for_request(context["request"]).root_page
+    print("this is the navvvv --------->>",nav)
+    for menu in nav.get_children().live():
+        print('this is menu--------->>>>',menu)
+        for submenu in menu.get_children().live():
+            print("submenu--->",submenu) 
+
+
     return Site.find_for_request(context["request"]).root_page
