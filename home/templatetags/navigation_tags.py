27bbda96cc9587519cf29c9f2118dbd5fa2110cd
yousrafu <yousra09blida1995@gmail.com>
2025-12-13 13:21:03 +0100

    added serviess

--- home/templatetags/navigation_tags.py
+++ home/templatetags/navigation_tags.py
@@ -12,11 +12,11 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def get_site_root(context):
     nav =Site.find_for_request(context["request"]).root_page
-    print("this is the navvvv --------->>",nav)
-    for menu in nav.get_children().live():
-        print('this is menu--------->>>>',menu)
-        for submenu in menu.get_children().live():
-            print("submenu--->",submenu) 
+    # print("this is the navvvv --------->>",nav)
+    # for menu in nav.get_children().live():
+    #     # print('this is menu--------->>>>',menu)
+    #     # for submenu in menu.get_children().live():
+    #     #     # print("submenu--->",submenu) 
 
 
     return Site.find_for_request(context["request"]).root_page
